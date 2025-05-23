81aa879978693529035866bb82b5bf6ecd09cc3b
TroyKDuncan <troyduncanbusiness@gmail.com>
2025-05-23 14:05:54 -0500

    Added support for portfolio website

--- server.js
+++ server.js
@@ -31,5 +31,30 @@ app.post("/api/notify", async (req, res) => {
   }
 });
 
+app.post("/api/portfolio-notification", async (req, res) => {
+  const { firstName, lastName, email } = req.body;
+
+  try {
+    await axios.post(
+      "https://ntfy.sh/portfolio-forms",
+      `Name: ${firstName} ${lastName}\nEmail: ${email}`,
+      {
+        headers: {
+          Title: "NEW PORTFOLIO FORM SUBMISSION",
+          Priority: "urgent",
+        },
+      }
+    );
+
+    console.log("Notification sent successfully");
+    res.status(200).json({ success: true });
+  } catch (error) {
+    console.error("Notification error:", error.message);
+    res
+      .status(500)
+      .json({ success: false, error: "Failed to send notification" });
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
